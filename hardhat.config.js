76014ec06c309f5ce3fdf320e21cf64267992df2
elwandas <elwandas@student.21-school.ru>
2024-12-11 16:44:53 +0300

    почистил директории, обновил gitignore и .json-файлы

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
